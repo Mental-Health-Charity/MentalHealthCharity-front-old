e617595ce11aa473c4fc9ecf6336da7ad19865b8
Jakub Lobos <saltver2@onet.eu>
2023-04-04 00:01:03 +0200

    feat: global mixins, about section, RWD fix: RWD

--- src/app/home/hero/heroimage/HeroImage.component.tsx
+++ src/app/home/hero/heroimage/HeroImage.component.tsx
@@ -5,14 +5,14 @@ interface HeroImageProps {
     src: StaticImageData,
     alt: string,
     width: number,
-    className?: string,
+    className?: string | undefined,
 }
 
 const HeroImage = ({src, width, alt, className}: HeroImageProps) => {
 
     return (
         <div className={`${styles.hero_image} ${className}`}>
-            <div>
+            <div>  {/* this div prevents from overflow*/}
                 <Image className={styles.hero_image__image} alt={alt} src={src} width={width} />
             </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import Hero from './home/hero/Hero.component';
+import About from './home/about/About.component';
 
 function Home() {
   return (
-    <Hero />
+    <>
+      <Hero />
+      <About />
+    </>
   )
 }
 
