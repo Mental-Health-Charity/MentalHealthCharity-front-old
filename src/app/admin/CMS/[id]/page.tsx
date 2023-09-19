fdc87a52c71e5139cbe03586d6bd98a0d9414a54
JakubLobos <saltver2@onet.eu>
2023-09-19 03:18:06 +0200

    fix: fix

--- src/app/admin/CMS/[id]/page.tsx
+++ src/app/admin/CMS/[id]/page.tsx
@@ -4,7 +4,7 @@ import { usePathname } from 'next/navigation';
 
 function CMSpage() {
   const id = usePathname();
-  const numericId = id ? parseInt(id.split('/').pop(), 10) : undefined;
+  const numericId = id ? parseInt(id.split('/').pop() || '', 10) : undefined;
   console.log('edit', numericId);
   return <CMS id={numericId} />;
 }
